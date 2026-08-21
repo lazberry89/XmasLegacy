7a547b50e39c1f2927390c09fb057acf713cc693
lazberry <lazberry89@gmail.com>
2026-08-22 04:03:27 +0900

    feat: stock functions added

--- paper/src/main/java/org/lazberry/xmaslegacy/role/general/RoleTool.java
+++ paper/src/main/java/org/lazberry/xmaslegacy/role/general/RoleTool.java
@@ -1,12 +1,37 @@
 package org.lazberry.xmaslegacy.role.general;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.lazberry.xmaslegacy.Roles.Role;
+import org.lazberry.xmaslegacy.ColorUtils;
+import org.lazberry.xmaslegacy.Roles.HiddenRoles;
 import org.lazberry.xmaslegacy.Roles.ServerRoles;
+import org.lazberry.xmaslegacy.XmasLegacy;
+import org.lazberry.xmaslegacy.settings.Annotation.Inject;
+import org.lazberry.xmaslegacy.settings.Annotation.Registry;
+import org.lazberry.xmaslegacy.settings.ServerType;
+import org.lazberry.xmaslegacy.utils.ItemBuilder;
 
 import java.util.EnumMap;
 import java.util.Map;
 
+@Registry.Include(type = ServerType.GLOBAL)
 public class RoleTool {
-    private final Map<Role, ItemStack> roleTool = new EnumMap<>();
+    private final Map<ServerRoles, ItemStack> roleTool = new EnumMap<>(ServerRoles.class);
+	private final Map<HiddenRoles, ItemStack> hiddenRoleTool = new EnumMap<>(HiddenRoles.class);
+	private final XmasLegacy plugin;
+	private final RoleManager rm;
+
+	@Inject
+	public RoleTool(XmasLegacy plugin, RoleManager rm) {
+		this.plugin = plugin;
+		this.rm = rm;
+	}
+
+	private ItemStack farmerTool() {
+		return ItemBuilder.of(plugin, Material.DIAMOND_HOE)
+				.setName(ColorUtils.chat(""))
+				.setLore(ColorUtils.chat())
+				.hideAllFlags()
+				.addEnchant()
+	}
 }
